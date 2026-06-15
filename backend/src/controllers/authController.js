b17adb508b94756c3ff2b2cf35643e4124a7782f
Lance <lanceandreiducante@gmail.com>
2026-06-15 13:32:52 +0800

    created logout controller

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -114,3 +114,16 @@ export const login = async (req, res) => {
     });
   }
 };
+
+export const logout = async (req, res) => {
+  res.clearCookie('token', {
+    httpOnly: true,
+    secure: false,
+    sameSite: 'lax',
+  });
+
+  return res.status(200).json({
+    success: true,
+    message: 'Account logout successfully.',
+  });
+};
