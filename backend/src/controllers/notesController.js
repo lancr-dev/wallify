a164b96f2ccba24a9ed01cf6a27e8bc4362cf494
Lance <lanceandreiducante@gmail.com>
2026-06-15 14:00:17 +0800

    created noteController

---
+++ backend/src/controllers/notesController.js
@@ -0,0 +1,29 @@
+import Note from '../models/WallifyNote';
+
+export const createNote = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+
+    if (!title || !content) {
+      return res.status(400).json({
+        success: false,
+        message: 'Title and Content are required.',
+      });
+    }
+
+    const note = await Note.create({
+      title,
+      content,
+    });
+
+    return res.status(201).json({
+      success: true,
+      data: note,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: `Error in createNote controller: ${error.message}`,
+    });
+  }
+};
